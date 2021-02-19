cf56cbe4d6a94e157113cf057d86e020985ce54c
LuanLeiteLeao <luanleiteraio@gmail.com>
2021-02-19 11:56:40 -0300

    busca por profundidade

---
+++ graph_of_states/graph.py
@@ -0,0 +1,38 @@
+from abstract_data_type.states_graph import State
+
+# Estados
+para = State('Pará')
+roraima = State('Roraima')
+mato_grosso = State('Mato Grosso')
+goias = State('Goiás')
+minas_gerais = State('Minas Gerais')
+sao_paulo = State('São Paulo')
+rio_de_janeiro = State('Rio de Janeiro')
+espirito_santo = State('Bahia')
+bahia = State('Bahia')
+sergipe = State('Sergipe')
+
+# Listas de adjacencias entre estados
+para.adjacencies = [mato_grosso]
+roraima.adjacencies = [mato_grosso]
+mato_grosso.adjacencies = [roraima, para, goias]
+goias.adjacencies = [mato_grosso, minas_gerais, bahia]
+minas_gerais.adjacencies = [goias, sao_paulo, rio_de_janeiro, espirito_santo]
+sao_paulo.adjacencies = [minas_gerais, rio_de_janeiro]
+rio_de_janeiro.adjacencies = [minas_gerais, sao_paulo, espirito_santo]
+espirito_santo.adjacencies = [bahia, rio_de_janeiro, minas_gerais]
+bahia.adjacencies = [goias, minas_gerais, espirito_santo, sergipe]
+sergipe.adjacencies = [bahia]
+
+graph = [
+    para,
+    roraima,
+    mato_grosso,
+    goias,
+    minas_gerais,
+    sao_paulo,
+    rio_de_janeiro,
+    espirito_santo,
+    bahia,
+    sergipe
+]
--- main.py
+++
@@ -1,5 +0,0 @@
-#grafo
-grafo_estados=[
-	"Estado1":["Estado2","Estado3"],
-	
-] 
